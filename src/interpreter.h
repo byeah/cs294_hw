7be9d93998b8266ebdbc2bec10b9a8132d650022
byeah <jby.yeah@gmail.com>
2015-09-04 19:38:49 -0700

    some header

--- src/interpreter.h
+++ src/interpreter.h
@@ -1,6 +1,60 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
+typedef enum{
+    Int,
+    Array,
+    Null
+} ObjType;
+
+typedef struct{
+    ObjType type;
+} Obj;
+
+typedef struct{
+    ObjType type;
+    int value;
+} IntObj;
+
+typedef struct{
+    ObjType type;
+    int length;
+    int* data;
+} ArrayObj;
+
+typedef struct{
+    ObjType type;
+} NullObj;
+
+
+typedef enum{
+    Var,
+    Func
+} EntryType;
+
+typedef struct {
+    EntryType type;
+} Entry;
+
+typedef struct {
+    EntryType type;
+    Obj* value;
+} VarEntry;
+
+typedef struct {
+    EntryType type;
+    ScopeStmt* stmt;
+    int numOfParas;
+    char** paraNames;
+} FuncEntry;
+
+typedef struct{
+    
+} EnvObj;
+
+
+
 void interpret (ScopeStmt* stmt);
 
 #endif
+
