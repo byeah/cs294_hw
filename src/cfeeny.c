4bec20bc19825647e2d46b0b0bb9910a71bfb9bf
Chang Lan <clan@eecs.berkeley.edu>
2015-09-15 15:05:37 -0700

    Import files for assignment 3.

--- src/cfeeny.c
+++ src/cfeeny.c
@@ -2,22 +2,31 @@
 #include<stdlib.h>
 #include<string.h>
 #include "utils.h"
-#include "ast.h"
-#include "interpreter.h"
+//#include "ast.h"
+//#include "interpreter.h"
+#include "bytecode.h"
+#include "vm.h"
 
 int main(int argc, char** argvs) {
 	//Check number of arguments
 	if (argc != 2) {
-		printf("Expected 1 argument to commandline.\n");
+		printf("Expected 2 argument to commandline.\n");
 		exit(-1);
 	}
 
 	//Read in AST
-	char* filename = argvs[1];
-	ScopeStmt* stmt = read_ast(filename);
+	//char* filename = argvs[1];
+	//ScopeStmt* stmt = read_ast(filename);
 
 	//Interpret
-	interpret(stmt);
+	//interpret(stmt);
+
+    //Read in bytecode
+    Program* p = load_bytecode(argvs[1]);
+    interpret_bc(p);
+    //  initvm(link_program(p));
+    //  runvm();
+    return 0;
 }
 
 
