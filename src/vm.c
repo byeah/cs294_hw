2bce47a43b6ee42c3e657ae9f2503e1b550d2107
Chang Lan <clan@eecs.berkeley.edu>
2015-09-16 15:39:57 -0700

    Basic instructions.

--- src/vm.c
+++ src/vm.c
@@ -195,10 +195,143 @@ void ht_clear(Hashtable *hashtable) {
     }
 }
 
+typedef enum {
+    Int,
+    Array,
+    Null,
+    Env
+} ObjType;
+
+typedef struct {
+    ObjType type;
+} Obj;
+
+typedef struct {
+    ObjType type;
+    int value;
+} IntObj;
+
+typedef struct {
+    ObjType type;
+} NullObj;
+
+typedef struct {
+    ObjType type;
+    int length;
+    Obj** data;
+} ArrayObj;
+
+inline
+int obj_type(Obj* o) {
+    return o->type;
+}
+
+static NullObj null_obj_singleton = { .type = Null };
+
+inline
+NullObj* make_null_obj() {
+    return &null_obj_singleton;
+}
+
+inline
+IntObj* make_int_obj(int value) {
+#ifdef DEBUG
+    ++ int_count;
+#endif
+    IntObj* o = malloc(sizeof(IntObj));
+    o->type = Int;
+    o->value = value;
+    return o;
+}
+
+inline
+IntObj* add(IntObj* x, IntObj *y) {
+    return make_int_obj(x->value + y->value);
+}
+
+inline
+IntObj* subtract(IntObj* x, IntObj *y) {
+    return make_int_obj(x->value - y->value);
+}
+
+inline
+IntObj* multiply(IntObj* x, IntObj *y) {
+    return make_int_obj(x->value * y->value);
+}
+
+inline
+IntObj* divide(IntObj* x, IntObj *y) {
+    return make_int_obj(x->value / y->value);
+}
+
+inline
+IntObj* modulo(IntObj* x, IntObj *y) {
+    return make_int_obj(x->value % y->value);
+}
+
+inline
+Obj* eq(IntObj* x, IntObj *y) {
+    return (x->value == y->value) ? (Obj*)make_int_obj(0) : (Obj*)make_null_obj();
+}
+
+inline
+Obj* lt(IntObj* x, IntObj *y) {
+    return (x->value < y->value) ? (Obj*)make_int_obj(0) : (Obj*)make_null_obj();
+}
+
+inline
+Obj* le(IntObj* x, IntObj *y) {
+    return (x->value <= y->value) ? (Obj*)make_int_obj(0) : (Obj*)make_null_obj();
+}
+
+inline
+Obj* gt(IntObj* x, IntObj *y) {
+    return (x->value > y->value) ? (Obj*)make_int_obj(0) : (Obj*)make_null_obj();
+}
+
+inline
+Obj* ge(IntObj* x, IntObj *y) {
+    return (x->value >= y->value) ? (Obj*)make_int_obj(0) : (Obj*)make_null_obj();
+}
+
+inline
+ArrayObj* make_array_obj(IntObj *length, Obj* init) {
+#ifdef DEBUG
+    ++ array_count;
+#endif
+    ArrayObj* o = malloc(sizeof(ArrayObj));
+
+    o->type = Array;
+    o->length = length->value;
+    o->data = malloc(sizeof(Obj*) * o->length);
+
+    for (int i = 0; i < o->length; i++) {
+        o->data[i] = init;
+    }
+    return o;
+}
+
+inline
+IntObj* array_length(ArrayObj* a) {
+    return make_int_obj(a->length);
+}
+
+inline
+NullObj* array_set(ArrayObj* a, IntObj* i, Obj* v) {
+    int index = i->value;
+    a->data[index] = v;
+    return make_null_obj();
+}
+
+inline
+Obj* array_get(ArrayObj* a, IntObj* i) {
+    return a->data[i->value];
+}
+
 // vm.c
 
 struct frame_t {
-    Vector* slots;
+    Vector* slots; // Vector of Obj*
     ByteIns* return_addr;
     struct frame_t* parent;
 };
@@ -226,6 +359,7 @@ static int pc = 0;
 static Vector* operand = NULL;
 static Frame* local_frame = NULL;
 
+static inline
 void vm_init(Program* p) {
     global_vars = ht_create(13);
     operand = make_vector();
@@ -237,7 +371,28 @@ void vm_init(Program* p) {
     pc = 0;
 }
 
+static inline 
+void push(Obj* obj) {
+    vector_add(operand, obj);
+}
 
+static inline
+void* pop() {
+    return vector_pop(operand);
+}
+
+static inline
+void* peek() {
+    return vector_peek(operand);
+}
+
+static inline
+void assert(int criteria, char *msg) {
+    if (!criteria) {
+        fprintf(stderr, msg);
+        exit(1);
+    }
+}
 
 void interpret_bc(Program* p) {
     vm_init(p);
@@ -247,9 +402,83 @@ void interpret_bc(Program* p) {
         ByteIns* ins = vector_get(code, pc);
         switch (ins->tag)
         {
-        default:
+        case LIT_OP: {
+            LitIns* lit_ins = (LitIns *)ins;
+            int index = lit_ins->idx;
+            Value* val = vector_get(p->values, index);
+            if (val->tag == INT_VAL) {
+                push(make_int_obj(((IntValue *)val)->value));
+            }
+            else if (val->tag == NULL_VAL) {
+                push(make_null_obj());
+            }
+            else {
+                assert(0, "Invalid object type for LIT.\n");
+            }
             break;
         }
+        case ARRAY_OP: {
+            Obj* init_val = pop();
+            IntObj* length = pop();
+            assert(length->type == Int, "Invalid length type for ARRAY.\n");
+            ArrayObj* obj = make_array_obj(length, init_val);
+            push(obj);
+            break;
+        }
+        case PRINTF_OP: {
+            PrintfIns* printf_ins = (PrintfIns *)ins;
+            int* res = malloc(sizeof(int) * (printf_ins->arity + 1));
+            for (int i = 0; i < printf_ins->arity; i++) {
+                IntObj* obj = pop();
+                assert(obj->type == Int, "Invalid object type for PRINTF.\n");
+                res[i] = obj->value;
+            }
+            int cur = printf_ins->arity;
+            StringValue *format = vector_get(p->values, printf_ins->format);
+            assert(format->tag == STRING_VAL, "Invalid object type for PRINTF_OP.\n");
+            for (char* p = format->value; *p && cur > 0; p++) {
+                if (*p != '~')
+                    printf("%c", *p);
+                else
+                    printf("%d", res[--cur]);
+            }
+            push(make_null_obj());
+            break;
+        }
+        case SET_LOCAL_OP: {
+            SetLocalIns *set_local_ins = (SetLocalIns *)ins;
+            vector_set(local_frame->slots, set_local_ins->idx, peek());
+            break;
+        }
+        case GET_LOCAL_OP: {
+            GetLocalIns *get_local_ins = (GetLocalIns *)ins;
+            push(vector_get(local_frame->slots, get_local_ins->idx));
+            break;
+        }
+        case SET_GLOBAL_OP: {
+            SetGlobalIns *set_global_ins = (SetGlobalIns *)ins;
+            StringValue *name = vector_get(p->values, set_global_ins->name);
+            assert(name->tag == STRING_VAL, "Invalid object type for SET_GLOBAL_OP.\n");
+            ht_put(global_vars, name->value, peek());
+            break;
+        }
+        case GET_GLOBAL_OP: {
+            GetGlobalIns *get_global_ins = (GetGlobalIns *)ins;
+            StringValue *name = vector_get(p->values, get_global_ins->name);
+            assert(name->tag == STRING_VAL, "Invalid object type for GET_GLOBAL_OP.\n");
+            push(ht_get(global_vars, name->value));
+            break;
+        }
+        case DROP_OP: {
+            pop();
+            break;
+        }
+        default: {
+            assert(0, "Invalid instruction.");
+            break;
+        }
+        }
+        ++pc;
     }
     
     printf("\n");
